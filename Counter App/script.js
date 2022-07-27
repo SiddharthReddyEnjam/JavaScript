ae22de6493e14937000ecae38871738e6beb3f88
SiddharthReddyEnjam <siddharthreddy8125@gmail.com>
2022-07-27 14:39:20 +0530

    added mousedown and up

--- Counter App/script.js
+++ Counter App/script.js
@@ -5,17 +5,17 @@ const input = document.getElementById("input");
 
 const valueDown = () => {
   let n = parseInt(input.value);
-  if (n > 0 || n <= -1) input.value = n - 1;
+  if (n > 0 || n <= -1) input.value = n--;
   if (n <= 0 && n > -1) {
     if (confirm("want negative value?")) {
-      input.value = n - 1;
+      input.value = n--;
     }
   }
 };
 
 const valueUp = () => {
   let n = parseInt(input.value);
-  input.value = n + 1;
+  input.value = n++;
 };
 buttondown.addEventListener("click", valueDown);
 buttonup.addEventListener("click", valueUp);
@@ -24,19 +24,46 @@ buttonup.addEventListener("click", valueUp);
 
 buttondown.addEventListener("click", valueD);
 buttonup.addEventListener("click", valueU);
+const span = document.getElementById("up_down");
 
 function valueD() {
-  const span = document.getElementById("up_down");
-  let n = parseInt(span.textContent);
-  span.textContent = n - 1;
+  let x = parseInt(span.textContent);
+  span.textContent = x--;
 }
 function valueU() {
-  const span = document.getElementById("up_down");
-  let n = parseInt(span.textContent);
-  span.textContent = n + 1;
+  let x = parseInt(span.textContent);
+  span.textContent = x++;
 }
 
 /////////////////////////////////
+let valuen = 0;
+let timer;
+
+function continuosIncerment() {
+  ++valuen;
+  span.innerHTML = valuen;
+  input.value = valuen;
+  timer = setTimeout(continuosIncerment, 200);
+}
+
+function continuosDecrement() {
+  --valuen;
+  span.innerHTML = valuen;
+  input.value = valuen;
+  timer = setTimeout(continuosDecrement, 200);
+}
+
+function timeoutClear() {
+  clearTimeout(timer);
+}
+
+buttondown.addEventListener("mousedown", continuosDecrement);
+buttondown.addEventListener("mouseup", timeoutClear);
+buttondown.addEventListener("mouseleave", timeoutClear);
+
+buttonup.addEventListener("mousedown", continuosIncerment);
+buttonup.addEventListener("mouseup", timeoutClear);
+buttonup.addEventListener("mouseleave", timeoutClear);
 
 ///////////////////////////////// responding to keys
 
