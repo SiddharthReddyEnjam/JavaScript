3bf64009725600fa0027da60a0e812fae72a99a2
SiddharthReddyEnjam <siddharthreddy8125@gmail.com>
2022-07-27 13:53:44 +0530

    Added Mousedown and Up

--- Counter App/script.js
+++ Counter App/script.js
@@ -35,3 +35,45 @@ function valueU() {
   let n = parseInt(span.textContent);
   span.textContent = n + 1;
 }
+
+/////////////////////////////////
+
+/////////////////////////////////
+
+window.addEventListener(
+  "keydown",
+  function (event) {
+    if (event.defaultPrevented) {
+      return; // Do nothing if the event was already processed
+    }
+
+    switch (event.key) {
+      case "ArrowDown":
+        // code for "down arrow" key press.
+        valueD();
+        valueDown();
+        break;
+      case "ArrowUp":
+        // code for "up arrow" key press.
+        valueU();
+        valueUp();
+        break;
+      case "ArrowLeft":
+        // code for "left arrow" key press.
+        valueD();
+        valueDown();
+        break;
+      case "ArrowRight":
+        // code for "right arrow" key press.
+        valueU();
+        valueUp();
+        break;
+      default:
+        return; // Quit when this doesn't handle the key event.
+    }
+
+    // Cancel the default action to avoid it being handled twice
+    event.preventDefault();
+  },
+  true
+);
